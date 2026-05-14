c4231fd642613f173eedd5093bb10e5fe961d6ae
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-14 15:19:58 +0300

    Date: 14.05.2026, Time: 58:19:15 Files generated:  test_1_14_05_2026_30_19_15_bool_assign_to_if.c,  test_2_14_05_2026_36_19_15_bool_assign_to_if.c,  test_3_14_05_2026_42_19_15_bool_assign_to_if.c,  test_4_14_05_2026_48_19_15_bool_assign_to_if.c,  test_5_14_05_2026_55_19_15_bool_assign_to_if.c

---
+++ boolean_to_if/test_2_14_05_2026_36_19_15_bool_assign_to_if.c
@@ -0,0 +1,9 @@
+int main() {
+    int a = 10;
+    int b = 20;
+    int f;
+    if (a > b) f = 1;
+    else f = 0;
+    printf("%d\n", f);
+    return 0;
+}
---
+++ boolean_to_if/test_4_14_05_2026_48_19_15_bool_assign_to_if.c
@@ -0,0 +1,9 @@
+int main() {
+    int a = 5;
+    int b = 3;
+    int f;
+    if (a > 0 && b > 0) f = 1;
+    else f = 0;
+    printf("%d\n", f);
+    return 0;
+}
