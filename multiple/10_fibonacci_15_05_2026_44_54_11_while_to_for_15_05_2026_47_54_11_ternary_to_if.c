a3485d6521a48de8d81543f4333206d6f2ddf279
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-15 11:54:51 +0300

    Date: 15.05.2026, Time: 50:54:11 Files generated:  10_fibonacci_15_05_2026_44_54_11_while_to_for_15_05_2026_47_54_11_ternary_to_if.c

---
+++ multiple/10_fibonacci_15_05_2026_44_54_11_while_to_for_15_05_2026_47_54_11_ternary_to_if.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int fib_recursive(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    return fib_recursive(n - 1) + fib_recursive(n - 2);
+}
+
+int fib_iterative(int n) {
+    if (n <= 0) return 0;
+    int a = 0;
+    int b = 1;
+    for (int i = 2; i <= n; i++) {
+        int tmp = a + b;
+        a = b;
+        b = tmp;
+    }
+    return b;
+}
+
+int is_fibonacci(int num) {
+    int a = 0;
+    int b = 1;
+    int found = (num == 0) ? 1 : 0;
+    for (; b < num;)  {
+        int tmp = a + b;
+        a = b;
+        b = tmp;
+        if (b == num) {
+            found = 1;
+        }
+    }
+    return found;
+}
+
+int main() {
+    for (int i = 0; i <= 10; i++) {
+        int rec = fib_recursive(i);
+        int iter = fib_iterative(i);
+        int same;
+        if (rec == iter) {
+            same = 1;
+        } else {
+            same = 0;
+        }
+        printf("fib(%d) = %d, match = %d\n", i, rec, same);
+    }
+    printf("Is 21 fib: %d\n", is_fibonacci(21));
+    printf("Is 22 fib: %d\n", is_fibonacci(22));
+    return 0;
+}
