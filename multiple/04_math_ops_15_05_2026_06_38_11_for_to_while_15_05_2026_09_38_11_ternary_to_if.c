da70144f9b57f2a3d3d12ec3759fb2e0e3cb1b75
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-15 11:38:12 +0300

    Date: 15.05.2026, Time: 12:38:11 Files generated:  04_math_ops_15_05_2026_06_38_11_for_to_while_15_05_2026_09_38_11_ternary_to_if.c

---
+++ multiple/04_math_ops_15_05_2026_06_38_11_for_to_while_15_05_2026_09_38_11_ternary_to_if.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int tmp = b;
+        b = a % b;
+        a = tmp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    int g = gcd(a, b);
+    int result = (a / g) * b;
+    return result;
+}
+
+int is_prime(int n) {
+    int prime = 1;
+    if (n < 2) {
+        prime = 0;
+    }
+    int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) {
+            prime = 0;
+            break;
+        }
+         i++;
+         }
+    return prime;
+}
+
+int power(int base, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result = result * base;
+    }
+    return result;
+}
+
+int main() {
+    int a = 12;
+    int b = 18;
+    printf("GCD(%d, %d) = %d\n", a, b, gcd(a, b));
+    printf("LCM(%d, %d) = %d\n", a, b, lcm(a, b));
+    int p;
+    if (is_prime(17)) {
+        p = 1;
+    } else {
+        p = 0;
+    }
+    printf("17 is prime: %d\n", p);
+    printf("2^10 = %d\n", power(2, 10));
+    return 0;
+}
