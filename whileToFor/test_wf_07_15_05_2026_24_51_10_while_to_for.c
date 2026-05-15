d41a934d95d741dbad773f9d2c1587479f1e4576
Смутин Даниил <Dsmutin2003@yandex.ru>
2026-05-15 10:51:40 +0300

    Date: 15.05.2026, Time: 39:51:10 Files generated:  test_wf_01_15_05_2026_57_50_10_while_to_for.c,  test_wf_02_15_05_2026_02_51_10_while_to_for.c,  test_wf_03_15_05_2026_07_51_10_while_to_for.c,  test_wf_04_15_05_2026_12_51_10_while_to_for.c,  test_wf_05_15_05_2026_17_51_10_while_to_for.c,  test_wf_07_15_05_2026_24_51_10_while_to_for.c,  test_wf_08_15_05_2026_30_51_10_while_to_for.c,  test_wf_09_15_05_2026_35_51_10_while_to_for.c

---
+++ whileToFor/test_wf_07_15_05_2026_24_51_10_while_to_for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i = 0;
+    for (; i < 3;) {
+        int j = 0;
+        while (j < 3) {
+            printf("%d %d\n", i, j);
+            j++;
+        }
+        i++;
+    }
+    return 0;
+}
